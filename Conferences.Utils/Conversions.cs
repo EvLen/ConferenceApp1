cc405bca56e9dc7dbc815b000d6c703a617921f7
steve <sleonard84@msn.com>
2014-12-09 22:25:49 +0000

    utils

---
+++ Conferences.Utils/Conversions.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System
+{
+    public static class Conversions
+    {
+
+        /// <summary>
+        /// Returns string empty if value is null
+        /// </summary>
+        public static string ConvertToString(this object value)
+        {
+            if (value == null)
+                return string.Empty;
+            else
+                return value.ToString();
+        }
+
+
+        public static int ConvertToInt32(this object value, int defVal)
+        {
+            if (value == null)
+                return defVal;
+            int retVal = defVal;
+            try { retVal = System.Convert.ToInt32(value); }
+            catch { }
+            return retVal;
+        }
+
+        public static int? ConvertToNullableInt32(this object value)
+        {
+            if (value == null)
+                return null;
+            int? retVal = null;
+            try
+            {
+                retVal = System.Convert.ToInt32(value);
+            }
+            catch { }
+            return retVal;
+        }
+
+
+        public static decimal ConvertToDecimal(this object value, decimal defVal)
+        {
+            if (value == null)
+                return defVal;
+            decimal retVal = defVal;
+            try
+            {
+                retVal = System.Convert.ToDecimal(value, new CultureInfo("en-US"));
+            }
+            catch { }
+            return retVal;
+        }
+
+        /// <summary>
+        /// Handles string representation of decimal numbers including currency and percentages
+        /// </summary>
+        public static decimal ConvertToDecimal(this string value, decimal defVal)
+        {
+            if (string.IsNullOrEmpty(value))
+                return defVal;
+            decimal retVal = defVal;
+            try
+            {
+                value = value.Replace("£", "").Replace("$", "").Replace("%", "").Replace("€", "");
+                retVal = System.Convert.ToDecimal(value, new CultureInfo("en-US"));
+            }
+            catch { }
+            return retVal;
+        }
+
+
+        public static decimal? ConvertToNullableDecimal(this object value)
+        {
+            if (value == null)
+                return null;
+            decimal? retVal = null;
+            try
+            {
+                retVal = System.Convert.ToDecimal(value, new CultureInfo("en-US"));
+            }
+            catch { }
+            return retVal;
+        }
+
+        /// <summary>
+        /// Handles string representation of decimal numbers including currency and percentages
+        /// </summary>
+        public static decimal? ConvertToNullableDecimal(this string value)
+        {
+            if (string.IsNullOrEmpty(value))
+                return null;
+            decimal? retVal = null;
+            try
+            {
+                value = value.Replace("£", "").Replace("$", "").Replace("%", "").Replace("€", "");
+                retVal = System.Convert.ToDecimal(value, new CultureInfo("en-US"));
+            }
+            catch { }
+            return retVal;
+        }
+
+
+        public static double ConvertToDouble(this object value, double defVal)
+        {
+            if (value == null)
+                return defVal;
+            double retVal = defVal;
+            try
+            {
+                retVal = System.Convert.ToDouble(value, new CultureInfo("en-US"));
+            }
+            catch { }
+            return retVal;
+        }
+
+
+        public static DateTime ConvertToDateTime(this object value, DateTime defVal)
+        {
+            if (value == null)
+                return defVal;
+            DateTime retVal = defVal;
+            try
+            {
+                DateTime? nullable = DateHelpers.GetCalculatedDateTime(value);
+                if (nullable == null)
+                    retVal = System.Convert.ToDateTime(value);
+                else
+                    retVal = nullable.Value;
+            }
+            catch { }
+            return retVal;
+        }
+
+
+        public static DateTime? ConvertToNullableDateTime(this object value)
+        {
+            if (value == null)
+                return null;
+            DateTime? retVal = null;
+            try
+            {
+                retVal = DateHelpers.GetCalculatedDateTime(value);
+                if (retVal == null)
+                    retVal = System.Convert.ToDateTime(value);
+            }
+            catch { }
+            return retVal;
+        }
+
+
+        public static bool ConvertToBool(this object value, bool defVal)
+        {
+            bool retVal = defVal;
+            try
+            {
+                if (value == null)
+                    return defVal;
+
+                if (value.ToString().Trim() == "1")
+                    retVal = true;
+                else if (value.ToString().Trim() == "0")
+                    retVal = false;
+                else if (value.ToString().Trim() == "on")
+                    retVal = true;
+                else if (value.ToString().Trim() == "checked")
+                    retVal = true;
+                else
+                    retVal = System.Convert.ToBoolean(value);
+            }
+            catch { }
+            return retVal;
+        }
+
+
+        public static bool? ConvertToNullableBool(this object value)
+        {
+            bool? retVal = null;
+            if (value == null)
+                return null;
+            try
+            {
+                if (value.ToString().Trim() == "1")
+                    retVal = true;
+                else if (value.ToString().Trim() == "0")
+                    retVal = false;
+                else if (value.ToString().Trim() == "on")
+                    retVal = true;
+                else if (value.ToString().Trim() == "checked")
+                    retVal = true;
+                else
+                    retVal = System.Convert.ToBoolean(value);
+            }
+            catch { }
+            return retVal;
+        }
+
+
+
+        public static decimal Invert(this decimal? item)
+        {
+            if (item != null)
+                return item.Value * -1;
+
+            return 0;
+
+
+        }
+
+        public static decimal Invert(this decimal item)
+        {
+
+            return item * -1;
+        }
+
+        public static string ToCurrencyPcl(this decimal? item)
+        {
+            return item != null ? item.Value.ToCurrencyPcl() : ((decimal)0).ToCurrencyPcl();
+        }
+
+        public static string ToCurrencyPcl(this decimal value)
+        {
+
+            return value.ToString("C");
+        }
+
+
+
+        public static string ToNumber(this decimal? item)
+        {
+            return item != null ? item.Value.ToNumber() : ((decimal)0).ToNumber();
+        }
+
+        public static string ToNumber(this decimal value)
+        {
+
+            return value.ToString("N");
+        }
+
+
+    }
+}
---
+++ Conferences.Utils/DateHelpers.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System
+{
+    public class DateHelpers
+    {
+        public static IEnumerable<DateTime> EachDay(DateTime from, DateTime thru)
+        {
+            for (var day = from.Date; day.Date <= thru.Date; day = day.AddDays(1))
+                yield return day;
+        }
+
+        public static DateTime GetDateDifference(DateTime startDate, DateTime endDate, ref int years, ref int months)
+        {
+
+
+            if (endDate < startDate)
+            {
+                years = 0;
+                months = 0;
+                return DateTime.Now;
+            }
+            TimeSpan Span = endDate - startDate;
+            DateTime diffDate = DateTime.MinValue + Span;
+
+            //The month can be one out as different months have different days in them. 
+            int daysInStartDateMonth = DateTime.DaysInMonth(startDate.Year, startDate.Month);
+            int daysInEndDateMonth = DateTime.DaysInMonth(endDate.Year, endDate.Month);
+            int diff = daysInEndDateMonth - daysInStartDateMonth;
+            diffDate = diffDate.AddDays(diff);//So adjust the diffDate manually
+
+            years = diffDate.Year - 1;
+            months = diffDate.Month - 1;
+            return diffDate;
+        }
+
+        /// <summary>
+        /// Attempts to create date from string representation such as +7days or -2months
+        /// </summary>
+        public static DateTime? GetCalculatedDateTime(object dateRepresentation)
+        {
+            try
+            {
+                string val = dateRepresentation.ToString().ToLower();
+                if (val == "now")
+                    return DateTime.Now;
+                int integerVal = 0;
+                bool isMinus = val.StartsWith("-");
+                bool isPlus = val.StartsWith("+");
+                val = val.Replace("-", "").Replace("+", "");
+                if ((isMinus) || (isPlus))
+                {
+                    if (val.Contains("days"))
+                    {
+                        integerVal = val.Replace("days", "").ConvertToInt32(0);
+                        if (integerVal != 0)
+                            return (isMinus) ? DateTime.Now.AddDays(-integerVal) : DateTime.Now.AddDays(integerVal);
+                    }
+                    else if (val.Contains("months"))
+                    {
+                        integerVal = val.Replace("months", "").ConvertToInt32(0);
+                        if (integerVal != 0)
+                            return (isMinus) ? DateTime.Now.AddMonths(-integerVal) : DateTime.Now.AddDays(integerVal);
+                    }
+                    else if (val.Contains("years"))
+                    {
+                        integerVal = val.Replace("years", "").ConvertToInt32(0);
+                        if (integerVal != 0)
+                            return (isMinus) ? DateTime.Now.AddYears(-integerVal) : DateTime.Now.AddDays(integerVal);
+                    }
+                }
+
+                return null;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        public static DateTime GetMondayThisWeek()
+        {
+            int substract = (1 - (int)DateTime.Now.DayOfWeek);
+            DateTime dt = DateTime.Now.AddDays(substract);
+            return new DateTime(dt.Year, dt.Month, dt.Day, 0, 0, 0);
+        }
+
+
+        public static TimeSpan GetWeeks(int weeks)
+        {
+            return TimeSpan.FromDays(weeks * 7);
+        }
+
+        public static TimeSpan GetDays(int days)
+        {
+            return TimeSpan.FromDays(days);
+        }
+
+        public static TimeSpan GetHours(int hours)
+        {
+            return TimeSpan.FromHours(hours);
+        }
+
+        public static TimeSpan GetMinutes(int minutes)
+        {
+            return TimeSpan.FromMinutes(minutes);
+        }
+    }
+}
