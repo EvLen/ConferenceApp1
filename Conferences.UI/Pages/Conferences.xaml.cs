c6c5a29b3fa1bd3bcab9a8cd079a64353d72c986
steve <sleonard84@msn.com>
2015-01-14 12:14:01 +0000

    added conferences xaml

---
+++ Conferences.UI/Pages/Conferences.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Conferences.Domain;
+using FirstFloor.ModernUI.Presentation;
+
+namespace Conferences.UI.Pages
+{
+    /// <summary>
+    /// Interaction logic for Conferences.xaml
+    /// </summary>
+    public partial class Conferences : UserControl
+    {
+        public Conferences()
+        {
+            InitializeComponent();
+            RefreshConferences();
+        }
+
+       
+
+        public void RefreshConferences()
+        {
+            var conferences = DBHelper.DB.Session.CreateCriteria(typeof(Conference)).List<Conference>();
+            ConferencesList.Links.Clear();
+            foreach (var c in conferences)
+                ConferencesList.Links.Add(new Link() { DisplayName = c.Name, Source = new Uri("/" + c.Id, UriKind.Relative) });
+
+            ConferencesList.Links.Add(new Link() { DisplayName = "Create New", Source = new Uri("/0", UriKind.Relative) });
+        }
+    }
+}
